23a388ecb7561bd4b11545595aea2ebe51807b7e
i-safronov <ilya.safa1337w@gmail.com>
2024-02-15 22:01:41 -0800

    Done

--- app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
@@ -1,11 +1,38 @@
 package ru.easycode.zerotoheroandroidtdd
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import androidx.core.view.isVisible
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var button: Button
+    private lateinit var textView: TextView
+
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        button = findViewById(R.id.hideButton)
+        textView = findViewById(R.id.titleTextView)
+        textView.visibility = if (savedInstanceState?.getBoolean(KEY) != false) View.VISIBLE
+        else View.GONE
+        button.setOnClickListener {
+            textView.visibility = View.GONE
+        }
     }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putBoolean(KEY, textView.isVisible)
+    }
+
+    companion object {
+        private const val KEY = "key"
+    }
+
 }
