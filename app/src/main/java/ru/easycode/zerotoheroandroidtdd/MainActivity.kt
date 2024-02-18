894de1513d4025373c96da37ec4d675df14508c1
i-safronov <ilya.safa1337w@gmail.com>
2024-02-18 14:07:53 -0800

    Done easy way

--- app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
@@ -11,7 +11,6 @@ import java.io.Serializable
 class MainActivity : AppCompatActivity() {
 
     private val count: Count = Count.Base()
-    private var viewState: ViewState = ViewState.Base()
 
     private lateinit var countTextView: TextView
     private lateinit var incrementButton: Button
@@ -23,54 +22,8 @@ class MainActivity : AppCompatActivity() {
         incrementButton = findViewById(R.id.incrementButton)
 
         incrementButton.setOnClickListener {
-            viewState.increment(textView = countTextView, count = count)
+            countTextView.text = count.increment(countTextView.text.toString())
         }
-    }
-
-    override fun onSaveInstanceState(outState: Bundle) {
-        super.onSaveInstanceState(outState)
-        outState.putSerializable(
-            KEY, viewState
-        )
-    }
-
-    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
-        super.onRestoreInstanceState(savedInstanceState)
-        viewState = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
-            savedInstanceState.getSerializable(KEY, ViewState::class.java) as ViewState
-        } else {
-            savedInstanceState.getSerializable(KEY) as ViewState
-        }
-        viewState.apply(textView = countTextView)
-    }
-
-    companion object {
-        const val KEY = "key"
-    }
-
-}
-
-interface ViewState : Serializable {
-
-    fun apply(textView: TextView)
-    fun increment(textView: TextView, count: Count)
-    fun number(): Int
-
-    class Base : ViewState {
-
-        private var number: Int = 0
-
-        override fun apply(textView: TextView) {
-            textView.text = number.toString()
-        }
-
-        override fun increment(textView: TextView, count: Count) {
-            number = count.increment(number = number.toString()).toInt()
-            apply(textView = textView)
-        }
-
-        override fun number(): Int = number
-
     }
 
 }
